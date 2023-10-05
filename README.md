9a32e0efe24335542dc38b70ce754978aebcad95
seboo <25958061+seboo@users.noreply.github.com>
2023-10-05 12:24:17 +0200

    LUT-26427 : add mock

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# library-notificationstore
+## library-notificationstore
 Client library to request  GRU user notifications and demands
+
+# Usage 
+
+# Configuration
+
+Example of configuration with direct access : 
+
+    <!-- IHttpTransportProvider declarations -->
+    <bean id="notificationStore.httpAccessTransport" class="fr.paris.lutece.plugins.notificationstore.v1.web.rs.service.HttpAccessTransport" >
+		    <property name="ApiEndPointUrl">
+            <value>${myplugin.notificationStore.ApiEndPointUrl}</value>
+        </property>
+     </bean>
+
+    <bean id="notificationStore.restTransport.httpAccess" class="fr.paris.lutece.plugins.notificationstore.v3.web.rs.service.NotificationStoreTransportRest">
+        <constructor-arg ref="notificationStore.httpAccessTransport"/>
+    </bean>
+    
+    <!-- Notification Store Service impl -->
+    <bean id="notificationStore.notificationStoreService" class="fr.paris.lutece.plugins.notificationstore.v1.web.service.NotificationStoreService">
+        <constructor-arg ref="notificationStore.restTransport.httpAccess"/>
+    </bean>
+
+Example with API Manager config :
+
+    <!-- IHttpTransportProvider declarations -->
+    <bean id="notificationStore.httpAccessTransport" class="fr.paris.lutece.plugins.notificationstore.v1.web.rs.service.HttpApiManagerAccessTransport" >
+	<property name="ApiEndPointUrl">
+            <value>${myplugin.notificationStore.ApiEndPointUrl}</value>
+        </property>
+	<property name="AccessManagerEndPointUrl">
+            <value>${myplugin.notificationStore.AccessManagerEndPointUrl}</value>
+        </property>
+	<property name="AccessManagerCredentials">
+            <value>${myplugin.notificationStore.AccessManagerCredentials}</value>
+        </property>
+     </bean>
+
+    <bean id="notificationStore.restTransport.httpAccess" class="fr.paris.lutece.plugins.notificationstore.v1.web.rs.service.NotificationStoreTransportRest">
+        <constructor-arg ref="notificationStore.httpAccessTransport"/>
+    </bean>
+
+    <!-- Notification Store Service impl -->
+    <bean id="notificationStore.notificationStoreService" class="fr.paris.lutece.plugins.notificationstore.v1.web.service.NotificationStoreService">
+        <constructor-arg ref="notificationStore.restTransport.httpAccess"/>
+    </bean>
+
+Example for Mock configuration :
+
+    <!-- Mock store -->
+    <bean id="notificationStore.mock" class="fr.paris.lutece.plugins.notificationstore.v1.web.rs.service.NotificationStoreMock">
+    </bean>
+
+    <!-- Notification Store Service impl -->
+    <bean id="notificationStore.notificationStoreService" class="fr.paris.lutece.plugins.notificationstore.v1.web.service.NotificationStoreService">
+        <constructor-arg ref="notificationStore.mock"/>
+    </bean>
---
+++ src/java/fr/paris/lutece/plugins/notificationstore/v1/web/rs/service/NotificationStoreMock.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2002-2023, City of Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.notificationstore.v1.web.rs.service;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+
+import fr.paris.lutece.plugins.grubusiness.business.customer.Customer;
+import fr.paris.lutece.plugins.grubusiness.business.demand.Demand;
+import fr.paris.lutece.plugins.grubusiness.business.notification.MyDashboardNotification;
+import fr.paris.lutece.plugins.grubusiness.business.notification.Notification;
+import fr.paris.lutece.plugins.grubusiness.business.web.rs.DemandDisplay;
+import fr.paris.lutece.plugins.grubusiness.business.web.rs.DemandResult;
+import fr.paris.lutece.plugins.grubusiness.business.web.rs.NotificationResult;
+import fr.paris.lutece.plugins.notificationstore.v1.web.service.INotificationStoreTransportProvider;
+
+/**
+ * 
+ * NotificationStoreTransportRest
+ *
+ */
+public class NotificationStoreMock implements INotificationStoreTransportProvider
+{
+
+    /**
+     * Logger
+     */
+    private static Logger _logger = Logger.getLogger( NotificationStoreMock.class );
+
+    /**
+     * Simple Constructor
+     */
+    public NotificationStoreMock( )
+    {
+    }
+
+    @Override
+    public DemandResult getListDemand( String strCustomerId, String strIdDemandType, String strIndex, String strNotificationType )
+    {
+        _logger.debug( "MOCK : Get list of demand for customer id " + strCustomerId );
+        return getMockDemandResult(  strCustomerId, strIdDemandType, null );
+    }
+
+    @Override
+    public DemandResult getListOfDemandByStatus( String strCustomerId, String strListStatus, String strIdDemandType, String strIndex, String strNotificationType )
+    {
+        _logger.debug( "MOCK : Get list of demand by status for customer id " + strCustomerId );
+        return getMockDemandResult(  strCustomerId, strIdDemandType, strListStatus );
+
+    }
+
+    @Override
+    public NotificationResult getListNotification( String strCustomerId, String strIdDemand, String strIdDemandType )
+    {
+        _logger.debug( "MOCK : Get list of notification of demand id " + strIdDemand );
+        return getMockListNotification( strCustomerId, strIdDemand, strIdDemandType );
+    }
+
+    @Override
+    public String getDemandTypes( )
+    {
+        _logger.debug( "MOCK: Get list of demand type " );
+        return getDemandTypesAsJson( );
+    }
+
+
+    /**
+     * get a list of 3 Mock Demands
+     * @param strCustomerId
+     * @param strIdDemandType
+     * @param strStatusList
+     * @return the demandResult list
+     */
+    private DemandResult getMockDemandResult( String strCustomerId, String strIdDemandType, String strStatusList ) {
+        DemandResult demandResult = new DemandResult();
+        demandResult.setListDemandDisplay( new ArrayList<DemandDisplay>( ) );
+
+        Customer customer = new Customer();
+        customer.setId( strCustomerId );
+        customer.setConnectionId( "AZERTY-AZERTY-AZERTY-AZERTY" );
+
+        for (int i=0; i<3; i++) {
+
+            String status = "EN COURS";
+            if ( strStatusList != null && strStatusList.split(",").length > 0 ) {
+                status = strStatusList.split(",")[0];
+            }
+            demandResult.getListDemandDisplay( ).add( getOneMockDemandDisplay( i, strIdDemandType!=null?strIdDemandType:"123",
+                    1672531200000L + (86400000L*i),  // =>  1/1/2023  + i days
+                    1685836800000L + (86400000L*i) , // => 4/6/2023 + i days
+                    status,
+                    customer ) );
+        }
+
+        return demandResult;
+
+    }
+
+    /**
+     * get mock list of 3 notifications
+     * @param strCustomerId
+     * @param strIdDemand
+     * @param strIdDemandType
+     * @return the Notification Result   list
+     */
+    private NotificationResult getMockListNotification(String strCustomerId, String strIdDemand, String strIdDemandType) {
+        NotificationResult notificationResult = new NotificationResult();
+
+        for (int i=0; i<3; i++) {
+
+            notificationResult.getNotifications().add(
+                    getOneMockNotification( 1685836800000L + (86400000L*i), // => 4/6/2023 + i days
+                    "Mock subject " + i,
+                    "Mock message " + i,
+                    "EN COURS") );
+        }
+
+        return notificationResult;
+    }
+
+    /**
+     * get a mock notification
+     *
+     * @param date
+     * @param subject
+     * @param message
+     * @param status
+     * @return the notification
+     */
+    private Notification getOneMockNotification( long date, String subject, String message, String status ) {
+        MyDashboardNotification myDashboardNotification = new MyDashboardNotification();
+        myDashboardNotification.setSubject( subject );
+        myDashboardNotification.setMessage( message );
+        myDashboardNotification.setStatusText( status );
+
+        Notification notification = new Notification();
+        notification.setDate( date );
+
+        notification.setMyDashboardNotification( myDashboardNotification );
+
+        return notification;
+    }
+
+    /**
+     * get one Mock DemandDisplay
+     * @param id
+     * @param demandType
+     * @param creationDate
+     * @param modifyDate
+     * @param status
+     * @return the DemandDisplay
+     */
+    private DemandDisplay getOneMockDemandDisplay( int id, String demandType, long creationDate, long modifyDate, String status, Customer customer ) {
+        Demand demand = new Demand();
+        demand.setDemandId( id );
+        demand.setTypeId( demandType );
+        demand.setCreationDate( creationDate );
+        demand.setModifyDate( modifyDate );
+        demand.setCustomer( customer );
+
+        DemandDisplay demandDisplay = new DemandDisplay();
+        demandDisplay.setDemand( demand );
+        demandDisplay.setStatus( status );
+
+        return demandDisplay;
+    }
+
+    /**
+     * get Mock demand type Ids as JSON
+     * @return a json string
+     */
+    private String getDemandTypesAsJson() {
+        return "[\n" +
+                "  {\n" +
+                "    \"id_demand_type\": 123,\n" +
+                "    \"label\": \"Paris Familles - Inscription scolaire\",\n" +
+                "    \"app_code\": \"A11\",\n" +
+                "    \"url\": \"A63\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"id_demand_type\": 234,\n" +
+                "    \"label\": \"demande de subventions\",\n" +
+                "    \"app_code\": \"B22\",\n" +
+                "    \"url\": \"W30\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"id_demand_type\": 456,\n" +
+                "    \"label\": \"Formulaires DSIN\",\n" +
+                "    \"app_code\": \"C33\",\n" +
+                "    \"url\": \"F56\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"id_demand_type\": 567,\n" +
+                "    \"label\": \"Formulaires DTEC\",\n" +
+                "    \"app_code\": \"D44\",\n" +
+                "    \"url\": \"F53\"\n" +
+                "  }\n" +
+                "]";
+    }
+
+}
